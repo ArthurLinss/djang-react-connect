203032a47ff5a597ed6dd4b93a36c16d4985bc7f
Linß, Arthur <arthur.linss@dlr.de>
2023-05-23 16:04:36 +0200

    initial commit with first running version

---
+++ project/app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
+
+from .models import *
+from rest_framework.response import Response
+from .serializer import *
+from django.http import Http404
+from rest_framework import status
+# Create your views here.
+
+
+class ReactView(APIView):
+    def get(self, request, format=None):
+        #output = [{"employee":output.employee, "department":output.department} for output in React.objects.all()]
+        #return Response(output)
+        reacts = React.objects.all()
+        serializer = ReactSerializer(reacts,many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            print("valid")
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
